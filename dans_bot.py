e23a05c3292ba3e2dae38f89d4ca7adee54762df
Daniel Enders <daniel.enders91@gmail.com>
2017-11-25 19:08:12 -0600

    added join capability, classes

--- dans_bot.py
+++ dans_bot.py
@@ -1,23 +1,102 @@
 import discord
+import asyncio
 import json
-from discord.ext.commands import Bot
 from discord.ext import commands
+from discord.ext.commands import Bot
+
 
 with open('.\credentials.json') as creds:
     credentials = json.load(creds)
 
+if not discord.opus.is_loaded():
+    discord.opus.load_opus('opus')
+
 Client = discord.Client()
 bot_prefix = "~"
-client = commands.Bot(command_prefix=bot_prefix)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(bot_prefix))
 
-@client.event
+class VoiceClip:
+    def __init__(self, message, player):
+        self.requester = message.author
+        self.channel = message.channel
+        self.player = player
+
+class VoiceState:
+    def __init__(self, bot):
+        self.current = None
+        self.voice = None
+        self.bot = bot
+        self.play_next_song = asyncio.Event()
+        self.songs = asyncio.Queue()
+        self.skip_votes = set() # a set of user_ids that voted
+        self.audio_player = self.bot.loop.create_task(self.audio_player_task())
+
+    def is_playing(self):
+        if self.voice is None or self.current is None:
+            return False
+
+        player = self.current.player
+        return not player.is_done()
+
+    @property
+    def player(self):
+        return self.current.player
+
+    def skip(self):
+        self.skip_votes.clear()
+        if self.is_playing():
+            self.player.stop()
+
+    def toggle_next(self):
+        self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
+
+    async def audio_player_task(self):
+        while True:
+            self.play_next_song.clear()
+            self.current = await self.songs.get()
+            await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
+            self.current.player.start()
+            await self.play_next_song.wait()
+
+class VoicePlayer:
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice_states = {}
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def summon(self, ctx):
+        """Summons the bot to join your voice channel."""
+        summoned_channel = ctx.message.author.voice_channel
+        if summoned_channel is None:
+            await self.bot.say('You are not in a voice channel.')
+            return False
+
+        state = self.get_voice_state(ctx.message.server)
+        if state.voice is None:
+            state.voice = await self.bot.join_voice_channel(summoned_channel)
+        else:
+            await state.voice.move_to(summoned_channel)
+
+        return True
+
+
+    def get_voice_state(self, server):
+        state = self.voice_states.get(server.id)
+        if state is None:
+            state = VoiceState(self.bot)
+            self.voice_states[server.id] = state
+
+        return state
+
+
+@bot.command(pass_context=True)
+async def voice_clip(ctx, clip):
+    await bot.say(clip)
+
+@bot.event
 async def on_ready():
     print ("Bot Online!")
-    print ("Name: {}".format(client.user.name))
-    print ("ID: {}".format(client.user.id))
-
-@client.command(pass_context=True)
-async def ping(ctx):
-    await client.say("Pong")
-
-client.run(credentials['client_token'])
+    print ("Name: {}".format(bot.user.name))
+    print ("ID: {}".format(bot.user.id))
+bot.add_cog(VoicePlayer(bot))
+bot.run(credentials['client_token'])
